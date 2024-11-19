6e2dd78a09ed6b29eaf311446c63e0f67657f1eb
Lined1 <108262570+Lined1@users.noreply.github.com>
2024-11-19 15:23:34 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 12.16 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/README.md
+++ 프로그래머스/2/12939. 최댓값과 최솟값/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최댓값과 최솟값 - 12939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=java) 
 
 ### 성능 요약
 
-메모리: 79.9 MB, 시간: 12.02 ms
+메모리: 87.8 MB, 시간: 12.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 19일 15:08:29
+2024년 11월 19일 15:23:31
 
 ### 문제 설명
 
--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -2,18 +2,14 @@ import java.util.Arrays;
 class Solution {
     public String solution(String s) {
         String[] input = s.split(" ");
-        String big = input[0];
-        String small = input[input.length-1];
-        for(int i=0;i<input.length;i++){
-            if(Integer.valueOf(big)<Integer.valueOf(input[i])){
-                big = input[i];
-            }
-            if(Integer.valueOf(small)>Integer.valueOf(input[i])){
-                small = input[i];
-            }
-        }
+        int[] nums = new int[input.length];
         
-        String answer = small+" "+big;
+        for(int i=0;i<input.length;i++){
+            nums[i]=Integer.valueOf(input[i]);
+        }
+        Arrays.sort(nums);
+        String answer = nums[0]+" "+nums[nums.length-1];
+        
         return answer;
     }
 }
