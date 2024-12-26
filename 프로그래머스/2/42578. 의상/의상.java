0acefb0aae51e04b82362b2b0b6cab5ec814a486
linedj <108262570+linedj@users.noreply.github.com>
2024-12-26 21:16:40 +0900

    [level 2] Title: 의상, Time: 0.14 ms, Memory: 89.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int solution(String[][] clothes) {
+        int len = clothes.length;
+        Map<String, Integer> matches = new HashMap<>();
+        
+        for(int i = 0; i < len; i++) {
+            String item = clothes[i][0];
+            String kind = clothes[i][1];
+            matches.put(kind, matches.getOrDefault(kind, 0) + 1);
+        }
+        int answer = 1;
+        
+        for(String key: matches.keySet()) {
+            answer *= matches.get(key) + 1;
+        }
+
+        return answer - 1;
+        
+    }
+}
