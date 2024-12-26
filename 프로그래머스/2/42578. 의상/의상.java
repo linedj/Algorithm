6d59964e05fbb489f0c203dc51181d9f7da77709
linedj <108262570+linedj@users.noreply.github.com>
2024-12-26 21:21:48 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,20 +1,22 @@
 import java.util.*;
+
 class Solution {
     public int solution(String[][] clothes) {
         int len = clothes.length;
-        Map<String, Integer> matches = new HashMap<>();
+        HashMap<String, Integer> matches = new HashMap<>();
         
         for(int i = 0; i < len; i++) {
-            String item = clothes[i][0];
-            String kind = clothes[i][1];
-            matches.put(kind, matches.getOrDefault(kind, 0) + 1);
+            String item = clothes[i][0];    //의상 이름
+            String kind = clothes[i][1];    //의상 종류
+            //KEY = 의상 종류, VALUE = 같은 종류를 같은 옷의 개수
+            matches.put(kind, matches.getOrDefault(kind, 0) + 1); 
         }
+        
         int answer = 1;
         
-        for(String key: matches.keySet()) {
+        for(String key: matches.keySet()) { //조합 경우의 수
             answer *= matches.get(key) + 1;
         }
-
         return answer - 1;
         
     }
