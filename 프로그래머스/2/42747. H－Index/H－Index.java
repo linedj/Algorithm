0e62f42c46454abe31e3a893a44f9c70903479ec
linedj <108262570+linedj@users.noreply.github.com>
2025-01-24 00:09:34 +0900

    [level 2] Title: H-Index, Time: 0.94 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,15 +1,16 @@
-import java.util.Arrays;
+import java.util.*;
+
 class Solution {
     public int solution(int[] citations) {
+        int answer = 0;
         Arrays.sort(citations);
-        int answer = citations.length;
-        for(int c: citations){
-            if(c >= answer){
+
+        for (int i = citations.length -1 ; i >= 0 ; i--) {
+            if(citations.length -i >= citations[i])
                 break;
-            }else{
-                answer -= 1;
-            }
+            answer++;
         }
+        
         return answer;
     }
 }
