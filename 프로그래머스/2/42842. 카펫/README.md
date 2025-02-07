8a0a8c8208d88d60455e9eab7a9fd0c173119151
linedj <108262570+linedj@users.noreply.github.com>
2025-02-07 10:00:50 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842#qna) 
 
 ### 성능 요약
 
-메모리: 96.2 MB, 시간: 0.04 ms
+메모리: 74.4 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 30일 20:59:18
+2025년 02월 07일 10:00:47
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,14 +1,23 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
 
-        int row=0;
-        while(true){
-            row++;
-            if(2*row*row-4*row-brown*row+2*yellow+2*brown==0) break;
+        int answer[] = new int[2]; // 가로, 세로
+
+        int sum = brown + yellow;
+
+        for(int i=3; i<sum; i++) { // 세로 3 이상
+            int j = sum / i; // i:세로, j:가로
+
+            if((sum % i != 0) || (j < 3)) continue; // 가로 3 이하이면 해당 X
+
+            // 옐로우 크기가 일치하면 리턴
+            if(((j-2)*(i-2)) == yellow) {
+                answer[0] = j;
+                answer[1] = i;
+                return answer;
+            }
         }
 
-        
-        int[] answer = {(brown-2*row)/2+2,row};
         return answer;
     }
 }
